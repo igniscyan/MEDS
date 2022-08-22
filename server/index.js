73fbee570b7fc9b796274c3004bf8582abc47247
Brandon Clark <bwclark97@gmail.com>
2022-08-21 21:13:43 -0400

    Added routes for getting patient info
    Co-authored-by: Boyd Duffie V <boydDuffie@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
@@ -10,57 +9,102 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const db = mysql.createPool({
-    host: 'mysql_db',
-    user: 'MYSQL_USER',
-    password: 'MYSQL_PASSWORD',
-    database: 'meds'
+  host: "mysql_db",
+  user: "MYSQL_USER",
+  password: "MYSQL_PASSWORD",
+  database: "meds",
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.get("/get/", (req, res) => {
+  const SelectQuery = " SELECT * FROM patients";
+  db.query(SelectQuery, (err, results) => {
+    res.send(results);
+  });
+});
 
+app.get("/get/:patientId", (req, res) => {
+  const patientId = req.params.patientId;
+  const SelectQuery = " SELECT * FROM patients WHERE patientId = ?";
+  db.query(SelectQuery, patientId, (err, results) => {
+    res.send(results);
+  });
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.post("/insert", (req, res) => {
+  const patientName = req.body.setPatientName;
+  const patientDOB = req.body.setPatientDOB;
+  const patientGender = req.body.setPatientGender;
+  const patientAddress = req.body.setPatientAddress;
+  const patientPhone = req.body.setPatientPhone;
+  const patientEmail = req.body.setPatientEmail;
 
+  const InsertQuery =
+    " INSERT INTO patients (patient_name, patient_dob, patient_gender, patient_address, patient_phone, patient_email) VALUES (?, ?, ?, ?, ?, ?)";
 
-app.get('/get/', (req, res) => {
-    const SelectQuery = " SELECT * FROM patients";
-    db.query(SelectQuery, (err, results) => {
-        res.send(results);
-    });
-})
-
-app.post('/insert', (req, res) => {
-    const patientName = req.body.setPatientName;
-    const patientDOB = req.body.setPatientDOB;
-    const patientGender = req.body.setPatientGender;
-    const patientAddress = req.body.setPatientAddress;
-    const patientPhone = req.body.setPatientPhone;
-    const patientEmail = req.body.setPatientEmail;
-
-    const InsertQuery = " INSERT INTO patients (patient_name, patient_dob, patient_gender, patient_address, patient_phone, patient_email) VALUES (?, ?, ?, ?, ?, ?)";
-
-    db.query(InsertQuery, [patientName, patientDOB, patientGender, patientAddress, patientPhone, patientEmail], (err, result) => {
-        if (err) console.log(err);
-    })
+  db.query(
+    InsertQuery,
+    [
+      patientName,
+      patientDOB,
+      patientGender,
+      patientAddress,
+      patientPhone,
+      patientEmail,
+    ],
+    (err, result) => {
+      if (err) console.log(err);
+    }
+  );
 });
 
 app.delete("/delete/:patientId", (req, res) => {
-    const patientId = req.params.patientId;
-    const DeleteQuery = "DELETE FROM patients WHERE patient_id = ?";
-    db.query(DeleteQuery, patientId, (err, result) => {
-        if (err) console.log(err);
-    })
+  const patientId = req.params.patientId;
+  const DeleteQuery = "DELETE FROM patients WHERE patient_id = ?";
+  db.query(DeleteQuery, patientId, (err, result) => {
+    if (err) console.log(err);
+  });
 });
 
-
-app.put("/update/:patientId", (req, res) => {
-    const patientUpdate = req.body.patientUpdate;
-    const patientId = req.params.patientId;
-    console.log("fudge knocka!")
+app.put("/update/:patientId/:", (req, res) => {
+  const patientUpdate = req.body.patientUpdate;
+  const patientId = req.params.patientId;
+  console.log("fudge knocka!");
 });
 
+app.post("/insert/patient_encounter/:patientId", (req, res) => {
+  console.log(req.body);
+  const patientEncountersTableFields = [
+    "patient_id",
+    "gyn",
+    "pregnant",
+    "last_menstrual_period",
+    "height",
+    "patient_weight",
+    "temp",
+    "systolic",
+    "diastolic",
+    "heart_rate",
+    "resp_rate",
+    "triage_note",
+    "med_note",
+    "pharm_note",
+    "eye_note",
+    "dental_note",
+    "goodies_note",
+    "location",
+    "open",
+  ];
+  const patiendtId = req.params.patientId;
+  const valuesArray = Object.values(req.body); //array of values from request
+  console.log(valuesArray);
+  valuesArray.unshift(patientId);
+  const insertPatientQuery = `INSERT INTO patient_encounters (${patientEncountersTableFields.join(
+    ","
+  )}) VALUES (${patientEncountersTableFields.forEach((field, idx) => "?,")});`;
+});
 
-
-app.listen('3001', () => { });
+app.listen("3001", () => {});
